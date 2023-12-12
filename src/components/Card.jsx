b603b79a662c2360b0cea1b130c47c1376c2dec5
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-12-12 19:10:33 +0000

    create a getPokemon function which retrieves pokemon sprites from API and caches them in localStorage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,3 @@
-export default function Card() {
-  return <div className="card"></div>;
+export default function Card({ handleClick }) {
+  return <div onClick={handleClick} className="card"></div>;
 }
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,10 +1,22 @@
 import Card from "../components/Card";
 
 export default function GamePage() {
+  const id = 2;
+  async function getPokemon(id) {
+    if (id in localStorage) {
+      console.log("retrieved");
+    } else {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+      const data = await response.json();
+      localStorage.setItem(id, data.sprites.front_default);
+      console.log("cached");
+    }
+  }
+
   return (
     <div className="game-page">
       <div className="card-display">
-        <Card></Card>
+        <Card handleClick={() => getPokemon(id)}></Card>
         <Card></Card>
         <Card></Card>
         <Card></Card>
