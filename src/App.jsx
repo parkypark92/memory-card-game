bfc904c9147b27ebb2909b067d6b8ea0a4d7e131
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-12-12 15:17:48 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState, useEffect } from "react";
+import GamePage from "./pages/GamePage";
+
+import "./styles/App.css";
+import "./styles/game-page.css";
+
+function App() {
+  return (
+    <div className="page-background">
+      <GamePage></GamePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/GamePage.jsx
@@ -0,0 +1,16 @@
+import Card from "../components/Card";
+
+export default function GamePage() {
+  return (
+    <div className="game-page">
+      <div className="card-display">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+    </div>
+  );
+}
