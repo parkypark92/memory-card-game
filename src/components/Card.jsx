5c3fb998f1d21c85d3ce71fe281e0558d1b8a566
Andrew <115462877+parkypark92@users.noreply.github.com>
2024-01-13 13:39:17 +0000

    add pokemon card image to backside of cards. Create test flip effect when card is hovered

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
 export default function Card({ handleClick, backgroundImage, dataID }) {
   return (
-    <div
-      data-id={dataID}
-      onClick={handleClick}
-      className="card"
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-      }}
-    ></div>
+    <div className="card-container">
+      <div className="card">
+        <div
+          data-id={dataID}
+          onClick={handleClick}
+          className="card-front"
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+          }}
+        ></div>
+        <div className="card-back"></div>
+      </div>
+    </div>
   );
 }
