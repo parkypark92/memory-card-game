709c9e41ee429d7b27dc3c70619fa5ba1200b774
Andrew <115462877+parkypark92@users.noreply.github.com>
2024-01-17 17:17:36 +0000

    add media query for smaller screen size, making cards smaller and centering scores

--- src/pages/start-page.jsx
+++ src/pages/start-page.jsx
@@ -3,13 +3,15 @@ export default function StartPage({ handleClick, removeClickedStatus }) {
 
   return (
     <div className="start-page">
-      <h2 className="start-message">
-        Click on each card only once. If you click on a card that has already
-        been clicked, it is game over!
-      </h2>
-      <button className="start-button" onClick={handleClick}>
-        Start Game
-      </button>
+      <div className="start-container">
+        <h2 className="start-message">
+          Click on each card only once. If you click on a card that has already
+          been clicked, it is game over!
+        </h2>
+        <button className="start-button" onClick={handleClick}>
+          Start Game
+        </button>
+      </div>
     </div>
   );
 }
